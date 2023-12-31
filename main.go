55c7eff7c2d6d57950bdfdd809ff2fac76ddb71e
IamSriKrishna <srik090704@gmail.com>
2023-12-31 15:31:15 +0530

    .env

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	Port := os.Getenv("PORT")
+
+	if Port == "" {
+		log.Fatal("Port is not found in the Environmnet")
+	}
+	fmt.Println("Port:", Port)
 }
